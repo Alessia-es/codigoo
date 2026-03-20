db25eddb43432586431ea46d326d3447afd12235
Alessia-es <alessiarojas57@gmail.com>
2026-03-20 16:54:24 -0500

    Refactor sorting logic and remove duplicates

--- app/controllers/temperatura_controller.py
+++ app/controllers/temperatura_controller.py
@@ -16,7 +16,6 @@ def guardar_temperatura(valor: float):
 
     return {"mensaje": "Guardado en DB", "valor": valor}
 
-list = [12,12,34,26,37,15]
 # 🔹 SELECT
 def obtener_datos():
     conn = get_connection()
@@ -29,16 +28,17 @@ def obtener_datos():
     conn.close()
 
     datos = [{"id": r[0], "valor": r[1]} for r in rows]
-   
+    list = [12,12,34,26,37,15]
+    def mostrar():
+        for i in range(n):
+            print(lis[i])
+    def ordenar():
+        n = len(list)
+        for i in range(n-1):
+            for j in range(i+1,n):
+                if list [i] > list [j]:
+                    aux = list [i]
+                    list [i] = list [j]
+                    list [j] = aux
+        mostrar()
     return {"datos": datos, "lista": list}
-    
-list = [12,12,34,26,37,15]
-def ordenar():
-    n = len(list)
-    for i in range(n-1):
-        for j in range(i+1,n):
-            if list [i] > list [j]:
-                aux = list [i]
-                list [i] = list [j]
-                list [j] = aux
-return {"lista ordenada:", list}
